b6a6cf092839805c0316d64ac4d4552a10f83b76
RuFrontEnd <k718198@gmail.com>
2021-10-02 10:02:36 +0800

    change Template html tag to styled component

--- src/template/Template.js
+++ src/template/Template.js
@@ -1,8 +1,10 @@
 import React from "react";
 import styled from "styled-components/macro";
 
+const Container = styled.section``;
+
 const Template = () => {
-  return <section></section>;
+  return <Container></Container>;
 };
 
 export default Template;
