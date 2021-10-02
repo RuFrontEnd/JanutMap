9cd1d970d841db388e55bf3a042b374bc58b3b7b
RuFrontEnd <k718198@gmail.com>
2021-10-02 11:55:45 +0800

    import reset.css / create variables

---
+++ src/variable/variable.js
@@ -0,0 +1,8 @@
+export const mainColor = "#F3F4F6";
+export const shallowMainColor = "#F4F5F7";
+export const lightReceivingColor = "#ffffff";
+export const shadowColor = "#d4dbea";
+export const textColor = "#373A43";
+export const placeholderColor = "#ACABB0";
+
+export const notoSans = '"Noto Sans", sans-serif';
