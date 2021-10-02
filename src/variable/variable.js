7e7c27d3eed247c1ad069ea0b520ee81f73df73f
RuFrontEnd <k718198@gmail.com>
2021-10-02 20:33:26 +0800

    clean code

--- src/variable/variable.js
+++ src/variable/variable.js
@@ -4,5 +4,4 @@ export const lightReceivingColor = "#ffffff";
 export const shadowColor = "#d4dbea";
 export const textColor = "#373A43";
 export const placeholderColor = "#ACABB0";
-
 export const notoSans = '"Noto Sans", sans-serif';
