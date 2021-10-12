5d4d3a7d62430b133c696b3c55973d7247d11b43
RuFrontEnd <k718198@gmail.com>
2021-10-12 19:24:30 +0800

    clean code

--- src/pages/GPSPostion.js
+++ src/pages/GPSPostion.js
@@ -8,6 +8,41 @@ import FullScreen from "layouts/FullScreen";
 import JauntButton from "components/JauntButton";
 import mapAnimation from "assets/map_animation.json";
 
+const GPSPostion = (props) => {
+  const { history } = props;
+  const $Lottie = useRef();
+
+  useEffect(() => {
+    lottie.loadAnimation({
+      container: $Lottie.current,
+      renderer: "svg",
+      loop: true,
+      autoplay: true,
+      animationData: mapAnimation,
+    });
+  }, []);
+
+  return (
+    <FullScreen>
+      <Background>
+        <Container>
+          <Lottie ref={$Lottie} />
+          <Hint> 請開啟定位，才能使用小旅行， 享有完整的使用體驗</Hint>
+          <JauntButton
+            text={"開啟定位"}
+            style={getLocationButtonStyle}
+            sharpRadius={false}
+            textStyle={getLocationButtonTextStyle}
+            onClick={() => {
+              history.push("/attractions");
+            }}
+          />
+        </Container>
+      </Background>
+    </FullScreen>
+  );
+};
+
 const Lottie = styled.div`
   width: 180px;
   height: 180px;
@@ -43,41 +78,6 @@ const getLocationButtonTextStyle = {
   background: "linear-gradient(to bottom, #38D0B7 100%, #35B3EA 100%)",
   webkitBackgroundClip: "text",
   webkitTextFillColor: "transparent",
-};
-
-const GPSPostion = (props) => {
-  const { history } = props;
-  const $Lottie = useRef();
-
-  useEffect(() => {
-    lottie.loadAnimation({
-      container: $Lottie.current,
-      renderer: "svg",
-      loop: true,
-      autoplay: true,
-      animationData: mapAnimation,
-    });
-  }, []);
-
-  return (
-    <FullScreen>
-      <Background>
-        <Container>
-          <Lottie ref={$Lottie} />
-          <Hint> 請開啟定位，才能使用小旅行， 享有完整的使用體驗</Hint>
-          <JauntButton
-            text={"開啟定位"}
-            style={getLocationButtonStyle}
-            sharpRadius={false}
-            textStyle={getLocationButtonTextStyle}
-            onClick={() => {
-              history.push("/attractions");
-            }}
-          />
-        </Container>
-      </Background>
-    </FullScreen>
-  );
 };
 
 export default withRouter(GPSPostion);
