bd67df4b898072dbe4b7a68475457ab04eb4b761
RuFrontEnd <k718198@gmail.com>
2021-10-14 20:11:22 +0800

    revise Template.js

--- src/template/Template.js
+++ src/template/Template.js
@@ -1,10 +1,11 @@
 import React from "react";
 import styled from "styled-components/macro";
 
-const Container = styled.section``;
-
-const Template = () => {
-  return <Container></Container>;
+const Template = (props) => {
+  const { className } = props;
+  return <Container className={className}></Container>;
 };
+
+const Container = styled.section``;
 
 export default Template;
