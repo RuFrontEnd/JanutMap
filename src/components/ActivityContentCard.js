a0215ce58d3f2b131b5e1ba8434a5bf20125fc44
RuFrontEnd <k718198@gmail.com>
2021-10-16 21:28:23 +0800

    style ActivityContentCard

--- src/components/ActivityContentCard.js
+++ src/components/ActivityContentCard.js
@@ -4,6 +4,7 @@ import ImageTmp from "assets/image_tmp.png";
 import { ReactComponent as CalendarRef } from "assets/calendar.svg";
 import { ReactComponent as Ticket } from "assets/ticket.svg";
 import { ReactComponent as Clock } from "assets/clock.svg";
+import { ReactComponent as Gps } from "assets/gps.svg";
 import { lightReceivingColor, shadowColor, blueGreen } from "variable/variable";
 import JauntButton from "components/JauntButton";
 
@@ -12,20 +13,29 @@ const ActivityContentCard = (props) => {
   return (
     <Container className={className}>
       <Image src={ImageTmp} />
-      <Data>
-        <Calendar />
-        2021.06.12~09.21
-      </Data>
-      <Price>
-        <Ticket />
-        $350
-      </Price>
+      <DateAndPrice>
+        <Data>
+          <Calendar />
+          2021.06.12~09.21
+        </Data>
+        <Price>
+          <Ticket />
+          $350
+        </Price>
+      </DateAndPrice>
       <Time>
         <Clock />
         周一~周日10:00 ~ 18:00(最後入場時間 17:30)
       </Time>
-      <Location>國立中正紀念堂 1展廳 (臺北市中正區中山南路21號)</Location>
-      <JauntButton text={"地圖"} />
+      <Location>
+        <Gps />
+        <Place>國立中正紀念堂 1展廳 (臺北市中正區中山南路21號)</Place>
+        <MapButton
+          text={"地圖"}
+          style={MapButtonStyle}
+          textStyle={MapButtonFontStyle}
+        />
+      </Location>
       <Introduction>
         曾被紐約時報譽為「世界上最偉大的裝飾藝術家」的慕夏，為20世紀新藝術代表人物之一，以女性的優美姿態結合花卉植物的繾綣曲線風靡世界，獨樹一幟的「慕夏風格」更影響後世多國的平面藝術家和設計師，更有「新藝術和現代平面設計之父」的美稱。
       </Introduction>
@@ -34,12 +44,21 @@ const ActivityContentCard = (props) => {
 };
 
 const Container = styled.section`
-  /* background-color: red; */
-  padding: 32px 32px 0px 32px;
+  width: 500px;
+  padding: 32px 32px 64px 32px;
   box-shadow: -3px -2px 2px ${lightReceivingColor}, 0px 2px 8px ${shadowColor};
 `;
 
-const Image = styled.img``;
+const Image = styled.img`
+  display: block;
+  margin: 0px auto 20px;
+`;
+
+const DateAndPrice = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+`;
 
 const Data = styled.div``;
 
@@ -51,10 +70,28 @@ const Calendar = styled(CalendarRef)`
 
 const Price = styled.div``;
 
-const Time = styled.div``;
+const Time = styled.div`
+  margin-bottom: 20px;
+`;
 
-const Location = styled.div``;
+const Location = styled.div`
+  display: flex;
+  margin-bottom: 25px;
+`;
 
-const Introduction = styled.p``;
+const Place = styled.div``;
+
+const Introduction = styled.p`
+`;
+
+const MapButton = styled(JauntButton)``;
+
+const MapButtonStyle = {
+  padding: "6px 11px",
+};
+
+const MapButtonFontStyle = {
+  fontSize: "18px",
+};
 
 export default ActivityContentCard;
